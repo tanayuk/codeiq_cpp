d4dc124b7845b1ed0715deb4ab6430cce6f7c736
Yuki Tanaka <yuki_tanaka@r.recruit.co.jp>
2016-04-12 21:03:11 +0900

    adding CPP project

---
+++ codeiq_cpp/q2791.cpp
@@ -0,0 +1,10 @@
+//
+//  q2791.cpp
+//  codeiq_cpp
+//
+//  Created by 田中　悠樹 on 4/12/16.
+//  Copyright © 2016 田中　悠樹. All rights reserved.
+//
+
+#include "q2791.hpp"
+
---
+++ codeiq_cpp/q2791.hpp
@@ -0,0 +1,14 @@
+//
+//  q2791.hpp
+//  codeiq_cpp
+//
+//  Created by 田中　悠樹 on 4/12/16.
+//  Copyright © 2016 田中　悠樹. All rights reserved.
+//
+
+#ifndef q2791_hpp
+#define q2791_hpp
+
+#include <stdio.h>
+
+#endif /* q2791_hpp */
