4ab2ecafc7883680b36ebf1747b64a4ba8d32693
Yuki Tanaka <yuki_tanaka@r.recruit.co.jp>
2016-04-12 21:00:00 +0900

    Initial Commit

---
+++ codeiq_cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  codeiq_cpp
+//
+//  Created by 田中　悠樹 on 4/12/16.
+//  Copyright © 2016 田中　悠樹. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
